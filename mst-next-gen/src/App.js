eacb23d26c934e32d539e0bd88ad284a33c03cd7
Groobin <79952458+Groobin@users.noreply.github.com>
2022-04-25 22:40:40 +1000

    Footer update

--- mst-next-gen/src/App.js
+++ mst-next-gen/src/App.js
@@ -11,7 +11,7 @@ import {
   Route,
   Link
 } from 'react-router-dom'
-import "./App.css";
+import Footer from './Footer'
 
 function App() {
   return (
@@ -33,6 +33,8 @@ function App() {
 
         <Route path="/" element={<HomePage/>}/>
       </Routes>
+
+      <Footer/>
     </div>
   );
 }
