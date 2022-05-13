da9cb382dcaa0c4cc6da1d3940ded0d2a5bd621b
anitafurtado <anitamfurtado@gmail.com>
2022-05-13 19:55:16 +1000

    CSS for Media and Blog Page

--- mst-next-gen/src/pages/MediaBlogPage.js
+++ mst-next-gen/src/pages/MediaBlogPage.js
@@ -5,7 +5,26 @@ import MediaAndBlog from "../MediaAndBlog.png"
 function MediaBlogPage(){
     return (
         <div>
-            <img src={MediaAndBlog} id="mediaAndBlogPrototype"/>
+            <div id="mediaBlogContainer">
+                <div id="blogContainer">
+                    <p class="mediaBlogTitle">Blog</p>
+                    <div id="blogPosts"></div>
+                </div>
+                <div id="mediaContainer">
+                    <p class="mediaBlogTitle">Media</p>
+                    <div id="mediaTiles"></div>
+                    <div id="mediaLivestreams">
+                    <div id="recentLivestreams">
+                        <p class="livestreamTitle">Recent Livestreams</p>
+                        <p class="livestreamListing"><a href="">December 2021</a></p>
+                    </div>
+                    <div id="upcomingLivestreams">
+                        <p class="livestreamTitle">Upcoming Livestreams</p>
+                        <p class="livestreamListing"><a href="">June 2022</a></p>
+                    </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
