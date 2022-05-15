f590b17c635ca1cd7b68f742dd9e3a0ed3dfa3e0
MendoMq <daniel.mendham@students.mq.edu.au>
2022-05-15 22:29:11 +1000

    Fixed refresh issue

--- mst-next-gen/src/index.js
+++ mst-next-gen/src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <Router>
+  <HashRouter>
     <App />
-  </Router>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
