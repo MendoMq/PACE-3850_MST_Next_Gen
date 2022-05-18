d8dfca3eae9dd5fab1ffec8ba3cb8cb6c4eb2b44
Groobin <79952458+Groobin@users.noreply.github.com>
2022-05-18 22:36:34 +1000

    about page content update

--- mst-next-gen/src/pages/AboutPage.js
+++ mst-next-gen/src/pages/AboutPage.js
@@ -24,18 +24,107 @@ function AboutPage(){
                       <p classname="committeeText">David Williamson is recognised as Australia’s most successful playwright. Over 40 years, his work has ranged across theatre, film and television, seen in Australia, Britain, the United States, Canada, Europe and Asia. After his successful play The Coming of Stork premiered in 1970, others have included The Removalists, What If You Died Tomorrow?, The Department and Travelling North. In 2014 a record of eight of his plays were professionally produced in Sydney alone. His plays have won numerous Australian and overseas awards, including the coveted George Devine Award for The Removalists. Many of David Williamson’s stage works have also been adapted for the screen, including Don’s Party, The Club, Travelling North and Sanctuary. He has won the Australian Film Institute film script award five times, and twelve Australian Writers’ Guild AWGIE Awards. His screenplay for On the Beach was nominated for the US Golden Globe awards. Many of Williamson’s plays were performed at Marian Street Theatre in the 1990s, and he is delighted to be a patron of the movement to re-open the theatre.</p>
                      </div>
                         </div>
+                
+                <div id="aboutContainer2">
+                 <div id="descriptiontext">
+                     <p>The Marian Street Theatre NextGen website is designed to bring new generations of the Marian Street Theatre community. We aim to build a stronger online presence for this community to communicate and bond over, engaging followers of all ages to participate. The site will act as a platform for our online content to be announced and accessed by official members while allowing them to utilise our site's features to be further engaged. MSTNG is made in hopes of encouraging everyone to come to theatres in person and enjoy the experience of a lifetime!</p>
+                 </div>
+                </div>
 
-                 <div id="aboutContainer2">
+                <div id="aboutContainer3">
+                 <div id="descriptiontext2">
+                     <p><p>COMITTEE</p></p>
+                 </div>
+                </div>
+
+                <div id="aboutContainer">
+                 <div id="descriptiontext3">
+                     <p>Our committee consists of experienced fundraisers, performing art professionals and concerned members of the community. They are committed to working together with Council, artists and the community to see Marian Street Theatre open again.</p>
+                 </div>
+                </div>
+
+                
+                 <div id="aboutContainer">
+                     <div id="firstContainer">
+                      <p className="committeeName">John Townend AM</p>
+                        <p classname="committeeText">ohn Townend AM, Emeritus FFIA, was a film, video and television director working with clients including advertising agencies, corporates and not for profits, locally, nationally and globally. For more than 25 years John was a certified fund-raising executive assisting many not for profit groups raise millions of dollars, including becoming chairman of a number of national and international diabetes organisations and the state and national Fundraising Institute.</p>
+                     </div>
+
+                     <div id="secondContainer">
+                      <p className="committeeName">Jacqueline Mees-Buss</p>
+                      <p classname="committeeText">Originally from the Netherlands, Jacqueline worked for large multinationals, both as an executive manager and as a consultant. She lived and worked in South Korea, the Philippines and Germany before settling in Killara in 1999.  In 2014 she obtained a PhD and became a lecturer in international business at the University of Sydney. She joined the SMST committee with her broad experience in marketing and business innovation, her unfailing desire to get this local theatre reopened, and strong belief that every community needs a theatre, a library and a playground.</p>
+                     </div>
+                  </div>
+
+                  <div id="aboutContainer">
+                     <div id="firstContainer">
+                      <p className="committeeName">Harry Loughnan</p>
+                        <p classname="committeeText">Harry Loughnan lives in Marian St Killara and has been on the Save Culworth Avenue Car Park Committee for a number of years. He has recently retired from Qantas where he worked for over 40 years as a training captain on various aircraft. He has been involved in mentoring and crew resource management.</p>
+                     </div>
+
+                     <div id="secondContainer">
+                      <p className="committeeName">Marianne Kopeinig</p>
+                      <p classname="committeeText">Marianne has over 20 years experience as a private practitioner and corporate counsel for private and ASX listed companies and broad industry experience in commercial, corporate governance, risk management and compliance functions.</p>
+                     </div>
+                  </div>
+
+                  <div id="aboutContainer">
                      <div id="firstContainer">
                       <p className="committeeName">Di Yerbury AO</p>
-                      <img src={profileplaceholder} width={"100%"} id="profileplaceholder"/>
                         <p classname="committeeText">Professor Emerita Dianne Yerbury AO is an Australian academic and university administrator. She was the Vice-Chancellor of Macquarie University from 1987 to 2005 and Australia’s first female Vice-Chancellor. In 1987 she was also made Professor Emerita at the University of New South Wales. She was named the 2002 Telstra New South Wales Business Woman of the Year.</p>
                      </div>
 
                      <div id="secondContainer">
-                      <p className="committeeName">Nadia Kostadinova</p>
-                      <img src={profileplaceholder} width={"100%"} id="profileplaceholder"/>
-                      <p classname="committeeText">Info to be added</p>
+                      <p className="committeeName">Gai Emmerson</p>
+                      <p classname="committeeText">Local Ku-ring-gai resident for 30 years. Working fulltime in Information Technology Business Applications.  Keen supporter of live theatre, cinema and reader of fiction. And a keen supporter of bringing Marian Street Theatre to life again.</p>
+                     </div>
+                  </div>
+
+                  <div id="aboutContainer">
+                     <div id="firstContainer">
+                      <p className="committeeName">Priscilla Yates</p>
+                        <p classname="committeeText">Professor Emerita Dianne Yerbury AO is an Australian academic and university administrator. She was the Vice-Chancellor of Macquarie University from 1987 to 2005 and Australia’s first female Vice-Chancellor. In 1987 she was also made Professor Emerita at the University of New South Wales. She was named the 2002 Telstra New South Wales Business Woman of the Year.</p>
+                     </div>
+
+                     <div id="secondContainer">
+                      <p className="committeeName">Wendy Blaxland</p>
+                      <p classname="committeeText">Wendy Blaxland has worked as an academic, teacher, writer and theatre professional. As well as publishing over 100 books mainly for children, she has written an unpublished history of Marian Street Theatre, is a long-term committee member of the Marian Street for Young People Board, and is a playwright and co-producer with Blaxland and Daughter Productions.</p>
+                     </div>
+                  </div>
+
+                  <div id="aboutContainer">
+                     <div id="firstContainer">
+                      <p className="committeeName">James Southwell</p>
+                        <p classname="committeeText">James Southwell is an actor and former student of Marian Street Theatre for Young People. In 2012 he came second in NSW in HSC drama, and in 2016 graduated with a distinction in a Bachelor of Arts majoring in Theatre and Performance Studies from UNSW. His next production will see him touring schools in NSW, QLD and ACT with Bard on the Beach doing Romeo and Juliet.</p>
+                     </div>
+
+                     <div id="secondContainer">
+                      <p className="committeeName">Jessica Blaxland Ashby</p>
+                      <p classname="committeeText">Jessica Blaxland Ashby is an arts professional working as co-producer for Blaxland and Daughter and General Manager for Marian Street Theatre for Young People. She has a long history with Marian Street Theatre, through her mother and grandmother, and through beginning her work in theatre there at the age of 10.</p>
+                     </div>
+                  </div>
+
+                  <div id="aboutContainer">
+                     <div id="firstContainer">
+                      <p className="committeeName">Michael Richardson</p>
+                        <p classname="committeeText">Michael Richardson is a former newspaper journalist, magazine publisher and editor and the author of several books on self-sufficiency and the environment.  He was first elected as State Member for The Hills and Castle Hill in 1993 and during an 18-year career served as Shadow Minister for Housing, Corrective Services and The Environment. He now lives in Killara.</p>
+                     </div>
+
+                     <div id="secondContainer">
+                      <p className="committeeName">Indu Balachandran</p>
+                      <p classname="committeeText">Indu Balachandran is the General Manager of Enterprises at the National Centre of Indigenous Excellence (NCIE). Indu has worked in enterprise development in the creative and social sectors for 15 years. Her work in the Indigenous sector was recognised by the UTS Human Rights Award for Reconciliation.  A keen contributor to cultural development in the South Asian diaspora, her film exploring gender and music, “Her Inner Song”, enjoyed national acclaim and prompted new dialogue in the community. Indu is a musician in the Indian Carnatic tradition, teaching and performing vocal music and the veena (a stringed instrument).</p>
+                     </div>
+                  </div>
+
+                  <div id="aboutContainer4">
+                     <div id="firstContainer">
+                      <p className="committeeName">Alicia Powell</p>
+                        <p classname="committeeText">Local resident for 50 years. Works full-time in Administrative Law. Keen supporter of emerging playwriting and literacy programs. Arts and theatre philanthropist. Passionate about preservation of Sydney’s theatre heritage and importance to social history. Longtime family association with The Australian Ballet.Fundraising experience in non-for-profit societies. Background in musical theatre. Currently writing play involving Marian Street Theatre history: The Scarlett Camellia Quest. Aims to see her childhood theatre reborn as a prominent beacon for all to Ku-ring-gai.</p>
+                     </div>
+
+                     <div id="secondContainer">
+                      <p className="committeeName">Luke Playoust</p>
+                      <p classname="committeeText">Luke Playoust is an architect with specialised expertise in all types of residential projects involving master planning and design. He has worked on projects ranging from heritage sensitive projects, renovations and restorations, to commercial fit out work and space planning.</p>
                      </div>
                   </div>
             </div>
