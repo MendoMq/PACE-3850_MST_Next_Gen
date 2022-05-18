8c8e722343d9a3de46481e0ae761fa34c9407c0f
anitafurtado <anitamfurtado@gmail.com>
2022-05-18 16:29:10 +1000

    Added login and register buttons in the header.

--- mst-next-gen/src/App.js
+++ mst-next-gen/src/App.js
@@ -16,10 +16,36 @@ import LoginPage from './Pages/LoginPage'
 import RegisterPage from './Pages/RegisterPage'
 import ForumDetail from './Pages/ForumDetail'
 
+import Button from '@mui/material/Button';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+    status: {
+      danger: '#f44336',
+    },
+    palette: {
+      primary: {
+        main: '#e02b20',
+      },
+    
+    },
+  });
+
 function App() {
   return (
     <div class="container">
-      <p>MSTNG Proudly supported by <a href="https://supportmarianstreettheatre.com/">Support Marian St Theatre</a></p>
+      <div id="headerLinks">
+        
+        <p style={{marginLeft: "10px"}}>MSTNG Proudly supported by <a href="https://supportmarianstreettheatre.com/">Support Marian St Theatre</a></p>
+        <div>
+          <ThemeProvider theme={theme}>
+              <Button variant="contained" color="primary" id="loginHeader" style={{maxWidth: '80px', maxHeight: '30px', minWidth: '80px', minHeight: '30px', fontSize: '16px', fontFamily: "'Abel', Helvetica, Arial, Lucida, sans-serif"}}component={Link} to="/login">Login</Button>
+              <Button variant="contained" color="primary" id="registerHeader" style={{maxWidth: '80px', maxHeight: '30px', minWidth: '80px', minHeight: '30px', fontSize: '16px', fontFamily: "'Abel', Helvetica, Arial, Lucida, sans-serif"}} component={Link} to="/register">Register</Button>
+          </ThemeProvider>
+        </div>
+      </div>
+      
       <div id="Header">
         <Link class="link" to="/">Home</Link>
         <Link class="link" to="/mediablog">Media and Blog</Link>
